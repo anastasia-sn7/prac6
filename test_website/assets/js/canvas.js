21052cbc9b64274a80c0f19cb62039144fc9badd
anonmy2021 <81091999+anonmy2021@users.noreply.github.com>
2021-10-26 01:43:46 +0300

    Update canvas.js

--- test_website/assets/js/canvas.js
+++ test_website/assets/js/canvas.js
@@ -14,7 +14,9 @@ function canvas(selector, options){
 
     let isPaint = false // чи активно малювання
     let points = [] //масив з точками
-
+    let color_change = 0;
+    let width_change = 0;
+    let color1, width1;
 // об’являємо функцію додавання точок в масив
     const addPoint = (x, y, dragging) => {
         // преобразуємо координати події кліка миші відносно canvas
@@ -31,8 +33,10 @@ function canvas(selector, options){
         context.clearRect(0, 0, context.canvas.width, context.canvas.height);
 
         context.strokeStyle = options.strokeColor;
+        if (color_change == 1) context.strokeStyle = color1;
         context.lineJoin = "round";
         context.lineWidth = options.strokeWidth;
+        if (width_change == 1) context.strokeStyle = width1;
         let prevPoint = null;
         for (let point of points){
             context.beginPath();
@@ -130,25 +134,48 @@ function canvas(selector, options){
     })
     toolBar.insertAdjacentElement('afterbegin', time)
 
-    const brushColor = document.createElement('button');
-    brushColor.classList.add('btn');
-    brushColor.textContent = 'Color';
+    const brushColor = document.createElement('input');
+    brushColor.setAttribute('type', 'color')
+    brushColor.setAttribute('value', options.strokeColor)
 
-    brushColor.addEventListener('click', changeColor);
+    brushColor.addEventListener('change', (e) => {
+        color1 = brushColor.value;
+        color_change = 1;
+    })
+    toolBar.insertAdjacentElement('afterend', brushColor)
 
-    function changeColor () {
-        //сделать смену цвета
-    }
+    const brushSize = document.createElement('input');
+    brushSize.setAttribute('type', 'range');
+    brushSize.setAttribute('value', options.strokeWidth);
+    brushSize.setAttribute('min',  options.strokeWidth);
+    brushSize.setAttribute('max',  options.strokeWidth + 25);
 
-    toolBar.insertAdjacentElement('afterbegin', brushColor)
+    brushSize.addEventListener('change', (e) => {
+        width1 = brushSize.value;
+        width_change = 1;
+    })
+    toolBar.insertAdjacentElement('afterend', brushSize)
 
-    const brushSize = document.createElement('button');
-    brushSize.classList.add('btn');
-    brushSize.textContent = 'Size';
+    const backgroundChange = document.createElement('button');
+    backgroundChange.classList.add('btn');
+    backgroundChange.textContent = 'Background';
 
-    brushSize.addEventListener('click', () => {
-
+    backgroundChange.addEventListener('click', () => {
+        const img = new Image;
+        img.src =`https://www.fillmurray.com/200/300)`;
+        img.onload = () => {
+            context.drawImage(img, 0, 0);
+        }
     })
 
-    toolBar.insertAdjacentElement('afterbegin', brushSize)
+    toolBar.insertAdjacentElement('afterbegin', backgroundChange)
+
+    time.innerHTML = '<img src="assets/img/clock.png" height="30px" width="30px" />';
+    save.innerHTML = '<img src="assets/img/save.png" height="30px" width="30px" />';
+    clearBtn.innerHTML = '<img src="assets/img/clear.png" height="30px" width="30px" />';
+    download.innerHTML = '<img src="assets/img/download.png" height="30px" width="30px" />';
+    restore.innerHTML = '<img src="assets/img/restore.png" height="30px" width="30px" />';
+    backgroundChange.innerHTML = '<img src="assets/img/image.png" height="30px" width="30px" />';
+
+    toolBar.insertAdjacentElement('afterbegin', ToolBarBtn)
 }
